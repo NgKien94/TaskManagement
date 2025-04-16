9588c1a59c17bf5eaf7ff32d0cd9289ddfc10f6b
NgKien94 <quangkienofqn@gmail.com>
2025-04-16 20:55:52 +0700

    Change the status of  an  existing  task

--- api/v1/controllers/task-controller.js
+++ api/v1/controllers/task-controller.js
@@ -2,51 +2,51 @@ const Task = require('../models/task-model');
 const paginationHelper = require('../../../helpers/pagination')
 const searchHelper = require('../../../helpers/search')
 // [GET] /tasks
-module.exports.index =  async (req,res) =>{
+module.exports.index = async (req, res) => {
     const find = {
         deleted: false
     }
-    
-    if(req.query.status){
+
+    if (req.query.status) {
         find.status = req.query.status;
     }
     // Search
     let objectSearch = searchHelper(req.query)
 
-    if(req.query.keyword){
-        find.title= objectSearch.regex
+    if (req.query.keyword) {
+        find.title = objectSearch.regex
     }
     // End Search
 
     // Pagination
     let initPagination = {
-        currentPage :1 ,
+        currentPage: 1,
         limitItems: 2
     }
     const countTasks = await Task.countDocuments(find);
-    const objectPagination = paginationHelper(initPagination,req.query,countTasks);
+    const objectPagination = paginationHelper(initPagination, req.query, countTasks);
     // End pagination
 
     // Sort
     const sort = {}
-    if(req.query.sortKey && req.query.sortValue){
+    if (req.query.sortKey && req.query.sortValue) {
         sort[req.query.sortKey] = req.query.sortValue;
     }
     // End sort 
 
-    
+
 
     const tasks = await Task.find(find)
-    .sort(sort)
-    .limit(objectPagination.limitItems)
-    .skip(objectPagination.skip)
-    ;
+        .sort(sort)
+        .limit(objectPagination.limitItems)
+        .skip(objectPagination.skip)
+        ;
     res.json(tasks)
 }
 
 
 // [GET] /tasks/detail/:id
-module.exports.detail = async (req,res) =>{
+module.exports.detail = async (req, res) => {
     try {
         const id = req.params.id;
         const record = await Task.findOne({
@@ -55,6 +55,32 @@ module.exports.detail = async (req,res) =>{
         })
         res.json(record)
     } catch (err) {
-       res.json({message: "Not found"})
+        res.json({ message: "Not found" })
     }
+}
+
+// [PATCH] /tasks/change-status/:id
+module.exports.changeStatus = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const status = req.body.status;
+
+        await Task.updateOne(
+            { _id: id },
+            {
+                $set: { status: status }
+            }
+        )
+
+        res.json({
+            code: 200,
+            message: "Cập nhật trạng thái thành công"
+        })
+    } catch (error) {
+        res.json({
+            code: 404,
+            message: "Lỗi tài nguyên không tồn tại"
+        })
+    }
+
 }
--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const port = process.env.PORT;
 const database = require('./config/database');
 database.connect();
 
+// Thêm dòng này để parse JSON body
+app.use(express.json());
+
+// Nếu dùng form-urlencoded (không cần trong JSON API thường dùng)
+app.use(express.urlencoded({ extended: true }));
 
 // Routes version 1
 const routesApiVer1 = require('./api/v1/routes/index-route')
