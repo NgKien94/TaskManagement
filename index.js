b36ca23b6af1181aa671d46eedfae601f2bb1b7e
NgKien94 <quangkienofqn@gmail.com>
2025-04-18 08:36:14 +0700

    Use cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors')
+
 
 // Configs
 
@@ -7,12 +9,17 @@ const app = express();
 const port = process.env.PORT;
 const database = require('./config/database');
 database.connect();
+// cors - Cấp quyền cho FrontEnd truy cập API từ BackEnd
+app.use(cors())
+
 
 // Thêm dòng này để parse JSON body
 app.use(express.json());
-
 // Nếu dùng form-urlencoded (không cần trong JSON API thường dùng)
 app.use(express.urlencoded({ extended: true }));
+
+
+
 
 // Routes version 1
 const routesApiVer1 = require('./api/v1/routes/index-route')
